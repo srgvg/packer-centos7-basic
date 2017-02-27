1c4e9b61974e605d5342770a1b42226a1e1a22c2
wimdk <wim.dekeyzer@gmail.com>
2017-02-27 15:00:52 +0100

    Update ssh-keys.sh

--- ssh-keys.sh
+++ ssh-keys.sh
@@ -3,7 +3,7 @@
 mkdir -p /home/admin/.ssh
 chmod 0700 /home/admin/.ssh
 cat > /home/admin/.ssh/authorized_keys <<EOF
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL1w5gHd6LAnUtpbiO0okp1xuhGBtJtmOtrl6w1X5Sdf serge@goldorak
+# insert authorized_key here, see readme.md for instructions
 EOF
 chmod 0600 /home/admin/.ssh/authorized_keys
 chown admin:admin -R /home/admin/.ssh
